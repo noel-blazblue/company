e1453019edf1872228982cdc989e562a9af09ae2
noel <noel872212444@163.com>
2019-05-29 19:01:15 +0800

    nine

--- yellow/js/index.js
+++ yellow/js/index.js
@@ -10,4 +10,15 @@ $(function() {
 
     var $tab_li = $('.recommend_list .tab_hd li')
     taggleClass($tab_li)
+
+    function taggleClass(ele,params) {
+        if(!params){
+            var params = 'active'
+        }
+        ele.on('click',function() {
+            $(this).siblings().removeClass(params)
+            $(this).addClass(params)
+            var $id = 
+        })
+    }
 })
